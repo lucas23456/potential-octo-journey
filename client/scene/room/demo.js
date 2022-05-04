61e6b59fcb1f35746a8ff899ab2e833f8ee76fe9
Glitch (slender-groovy-aquarius) <none>
2022-05-04 10:26:28 +0000

    ☺️⚾️ Checkpoint ./client/scene/room/demo.js:36613497/1963 ./client/scene/test-navmesh.gltf:36613497/336219

--- client/scene/room/demo.js
+++ client/scene/room/demo.js
@@ -1,20 +1,39 @@
+
 export function markup(
   /** @type {import("@notml/core").oom} */ oom,
   /** @type {import("@notml/core").OOMElementProxy} */ assets,
   /** @type {import("@notml/core").OOMElementProxy} */ scene
-) {
+                                            
+)
+
+
+
+{
   assets(oom
     .aAssetItem({
-      id: 'art-gallery-cage',
-      src: './scene/models/ArtGalleryCAGE.glb'
-    }))
-  scene(oom
-    .aBox({
-      staticBody: true,
-      position: '10 0 10',
-      width: '10',
-      height: '3',
-      depth: '10'
+      id: 'HallDeconstruct',
+      src: 'https://cdn.glitch.global/b5b15357-41a5-4881-a872-3cb6b05c0e48/FoldsMeshBuild.gltf?v=1651044548576'
     })
-    .aEntity({ gltfModel: '#art-gallery-cage', position: '15 0.25 7' }))
+  .aAssetItem({
+      id: 'Glass',
+      src: 'https://cdn.glitch.global/88fe0f09-858a-449e-936d-4ec30e06f635/GlassOuter.gltf?v=1650638051413'
+    })
+         .aAssetItem({
+      id: 'X-program',
+      src: 'https://cdn.glitch.global/88fe0f09-858a-449e-936d-4ec30e06f635/X-prog.gltf?v=1650704573096'
+    })
+  .aAssetItem({
+      id: 'GlassInner',
+      src: 'https://cdn.glitch.global/88fe0f09-858a-449e-936d-4ec30e06f635/GlassInner.gltf?v=1650638051773'
+    }))
+  
+  scene(oom
+    .aEntity({ id:'navigationmesh', navMesh: true, visible: false, gltfModel: '/scene/test-navmesh.gltf', position: '0 0 0', scale: '2 2 2'})
+    .aEntity({ gltfModel: '#HallDeconstruct', position: '0 0 0', scale: '2 2 2', enmap:'' })
+  //.aEntity({ gltfModel: '#Glass', position: '0 -1 0', scale: '2 2 2', enmap:''})
+  //.aEntity({ gltfModel: '#GlassInner', position: '0 -1 0', scale: '2 2 2', enmapinner:''})
+  //.aEntity({ gltfModel: '#X-program', position: '0 -1 0', scale: '2 2 2',enmapinner:'' })
+       )
+  
+  
 }
