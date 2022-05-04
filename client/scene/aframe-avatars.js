875b484377a6651e0c8649f771fe2ebc95e7c9a7
Glitch (slender-groovy-aquarius) <none>
2022-05-04 13:52:05 +0000

    🍛🎣 Checkpoint ./.glitch-assets:36613497/395 ./client/scene/aframe-avatars.js:36613497/971

--- client/scene/aframe-avatars.js
+++ client/scene/aframe-avatars.js
@@ -169,7 +169,12 @@ export function markup(
         rotation: '0 0 0'
       }, oom
                // Добавил курсор
-               .aCursor({visible:'true', geometry:'primitive: plane', src:'#cursorImage'})
+               .aCursor({visible:'true', geometry:'primitive: plane', material: 'src:#cursorImage'})
+               .aEntity({
+          class: 'nametag',
+          visible: 'false',
+          text: `value: ${userName}; align:center;`
+        })
         .aEntity({
           class: 'nametag',
           visible: 'false',
