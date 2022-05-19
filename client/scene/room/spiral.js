a265abd19aea4ff76aa20fb1eee1d128bc349014
Glitch (slender-groovy-aquarius) <none>
2022-05-19 18:20:03 +0000

    🙏🏘 Checkpoint ./client/scene/room/spiral.js:36613497/164

--- client/scene/room/spiral.js
+++ client/scene/room/spiral.js
@@ -46,6 +46,7 @@ export function markup(
   scene(oom
     
     .aEntity({ id: 'Navigationmesh', navMesh: true, visible: false,  gltfModel: '#Navigmesh', position: '0 0 0', scale: '2 2 2'})
+        .aEntity({ id: 'NavigationmeshSphere', navMesh: true, visible: false,  gltfModel: '#NavigmeshSphere', position: '0 0 0', scale: '2 2 2'})
     .aEntity({ gltfModel: '#HallDeconstruct', position: '0 0 0', scale: '2 2 2', enmap:'' })
     .aEntity({ gltfModel: '#HallDeconstruct2floor', position: '0 0 0', scale: '2 2 2', enmap:'' })
   .aEntity({ gltfModel: '#Glass', position: '0 0 0', scale: '2 2 2', enmapinner:''})
