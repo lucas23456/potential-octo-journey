c9c37365ea96ee33f45d9963dcdde3e4747d1e1d
Glitch (slender-groovy-aquarius) <none>
2022-05-19 18:08:11 +0000

    🏟🌜 Checkpoint ./.glitch-assets:36613497/1104 ./client/scene/room/spiral.js:36613497/1070 ./client/scene/spiralSphereNavMesh.gltf:36613497/306314

--- client/scene/room/spiral.js
+++ client/scene/room/spiral.js
@@ -14,14 +14,26 @@ export function markup(
       id: 'HallDeconstruct',
       src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/Spiral.gltf?v=1652719036449'
     })
+         
+    .aAssetItem({
+      id: 'HallDeconstruct2floor',
+      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/SpiralSphere.gltf?v=1652983032574'
+    })        
+         
   .aAssetItem({
       id: 'Glass',
       src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/Glass.gltf?v=1652719035389'
     })
          .aAssetItem({
-      id: 'Navigmesh',
+    id: 'Navigmesh',
       src: '/scene/spiralNavMesh.gltf'
     }) 
+         .aAssetItem({
+    id: 'NavigmeshSphere',
+      src: '/scene/spiralSphereNavMesh.gltf'
+    }) 
+         
+         
          .aAssetItem({
       id: 'X-program',
       src: 'https://cdn.glitch.global/88fe0f09-858a-449e-936d-4ec30e06f635/X-prog.gltf?v=1650704573096'
@@ -35,6 +47,7 @@ export function markup(
     
     .aEntity({ id: 'Navigationmesh', navMesh: true, visible: false,  gltfModel: '#Navigmesh', position: '0 0 0', scale: '2 2 2'})
     .aEntity({ gltfModel: '#HallDeconstruct', position: '0 0 0', scale: '2 2 2', enmap:'' })
+    .aEntity({ gltfModel: '#HallDeconstruct2floor', position: '0 0 0', scale: '2 2 2', enmap:'' })
   .aEntity({ gltfModel: '#Glass', position: '0 0 0', scale: '2 2 2', enmapinner:''})
   .aEntity({ gltfModel: '#GlassOuter', position: '0 0 0', scale: '2 2 2', enmap:''}))
   // .aEntity({ gltfModel: '#X-program', position: '-30 -1 28', scale: '2 2 2', enmapinner:'', link:'href: https://seasoned-discovered-mass.glitch.me' }))
