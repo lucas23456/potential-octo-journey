255c8321fd7af5684cdae80afa80699df16c1374
Glitch (slender-groovy-aquarius) <none>
2023-01-13 20:37:44 +0000

    🌲🎮 Checkpoint ./client/scene/aframe-world.js:36613497/39 ./.glitch-assets:36613497/406 ./client/scene/room/blackSpiral.js:36613497/1830

--- client/scene/room/blackSpiral.js
+++ client/scene/room/blackSpiral.js
@@ -12,7 +12,7 @@ export function markup(
   assets(oom
     .aAssetItem({
       id: 'BlackBox',
-      src: 'https://cdn.glitch.global/b1780a83-15c5-4154-bd07-deed2726dd55/BlackBoxGallery.gltf?v=1673639373519'
+      src: 'https://cdn.glitch.global/b1780a83-15c5-4154-bd07-deed2726dd55/BlackBoxGallery.gltf?v=1673641933318'
     })
              
          .aAssetItem({
@@ -26,16 +26,7 @@ export function markup(
     .aEntity({ id: 'Navigationmesh', navMesh: true, visible: false,  gltfModel: '#Navigmesh', position: '0 0 0', scale: '0.5 0.5 0.5'})
         // .aEntity({ id: 'NavigationmeshSphere', navMesh: true, visible: false,  gltfModel: '#NavigmeshSphere', position: '0 0 0', scale: '2 2 2'})
     .aEntity({ gltfModel: '#BlackBox', position: '0 0 0', scale: '0.5 0.5 0.5', enmap:'' })
-  .aEntity({ gltfModel: '#Glass', position: '0 0 0', scale: '2 2 2', enmapinner:''})
-  // .aEntity({ gltfModel: '#GlassOuter', position: '0 0 0', scale: '2 2 2', enmap:''})
-       .aEntity({ gltfModel: '#GlassWall0', position: '0 0 0', scale: '2 2 2', enmapinner:'',animation:'property: position; to: 0 0.1 0; dir: alternate; loop: true; dur: 2500; easing: linear'})
-        .aEntity({ gltfModel: '#GlassWall1', position: '0 0 0', scale: '2 2 2', enmapinner:'',animation:'property: position; to: 0 -0.1 0; dir: alternate; loop: true; dur: 2500; easing: linear'})
-        .aEntity({ gltfModel: '#GlassWall2', position: '0 0 0', scale: '2 2 2', enmapinner:'',animation:'property: position; to: 0 -0.12 0; dir: alternate; loop: true; dur: 2500; easing: linear'})
-        .aEntity({ gltfModel: '#GlassWall3', position: '0 0 0', scale: '2 2 2', enmapinner:'',animation:'property: rotation; to: 0 1 0; dir: alternate; loop: true; dur: 2500; easing: linear'})
-        .aEntity({ gltfModel: '#GlassWall4', position: '0 0 0', scale: '2 2 2', enmapinner:'',animation:'property: position; to: 0 -0.1 0; dir: alternate; loop: true; dur: 2500; easing: linear'})
-        .aEntity({ gltfModel: '#GlassWall5', position: '0 0 0', scale: '2 2 2', enmapinner:'',animation:'property: rotation; to: 0 -0.7 0; dir: alternate; loop: true; dur: 2500; easing: linear'})
-        .aEntity({ gltfModel: '#GlassWall6', position: '0 0 0', scale: '2 2 2', enmapinner:'',animation:'property: rotation; to: 0 -0.5 0; dir: alternate; loop: true; dur: 2500; easing: linear'})
-  // .aEntity({ gltfModel: '#SphereGlassOuter', position: '0 0 0', scale: '2 2 2', enmap:''})
+  
        
        )
   // .aEntity({ gltfModel: '#X-program', position: '-30 -1 28', scale: '2 2 2', enmapinner:'', link:'href: https://seasoned-discovered-mass.glitch.me' }))
