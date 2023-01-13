9479561bada83fcc145644c583303f12e3255772
Glitch (slender-groovy-aquarius) <none>
2023-01-13 21:37:42 +0000

    😄👍 Checkpoint ./client/scene.js:36613497/39 ./client/scene/aframe-world.js:36613497/6 ./.glitch-assets:36613497/406 ./client/scene/room/blackSpiral.js:36613497/198

--- client/scene.js
+++ client/scene.js
@@ -76,7 +76,7 @@ export async function markup(/** @type {import("@notml/core").oom} */ oom) {
   const scene = oom.aScene({
     id: 'main-scene',
     // Добавил renderer
-    renderer: 'antialias: true; colorManagement: false;',
+    renderer: 'antialias: true; colorManagement: true;',
     physics: 'gravity: -0.5;',
     networkedScene: `room: ${room}; debug: false; adapter: easyrtc; audio: ${hasMic};`
   }, assets)
