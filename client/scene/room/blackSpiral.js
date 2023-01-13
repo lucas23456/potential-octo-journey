c03b0220a3b64a89528634441509c7930c2fe389
Glitch (slender-groovy-aquarius) <none>
2023-01-13 19:38:14 +0000

    💇📫 Checkpoint ./.glitch-assets:36613497/363 ./client/scene/room/blackSpiral.js:36613497/2400

--- client/scene/room/blackSpiral.js
+++ client/scene/room/blackSpiral.js
@@ -11,75 +11,21 @@ export function markup(
 {
   assets(oom
     .aAssetItem({
-      id: 'HallDeconstruct',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/BlackCone.gltf?v=1654338428583'
+      id: 'BlackBox',
+      src: 'https://cdn.glitch.global/b1780a83-15c5-4154-bd07-deed2726dd55/BlackBoxGallery.gltf?v=1673638158045'
     })
-         
-    .aAssetItem({
-      id: 'HallDeconstruct2floor',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/SpiralSphere.gltf?v=1652983032574'
-    })        
-         
-  .aAssetItem({
-      id: 'Glass',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/ConeGlass.gltf?v=1654154756855'
-    })
-         .aAssetItem({
-      id: 'GlassWall0',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/BlackConeGlassWalls.gltf?v=1654271436101'
-    })
-         .aAssetItem({
-      id: 'GlassWall1',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/BlackConeGlassWalls1.gltf?v=1654271443556'
-    })
-         .aAssetItem({
-      id: 'GlassWall2',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/BlackConeGlassWalls2.gltf?v=1654271443348'
-    })
-         .aAssetItem({
-      id: 'GlassWall3',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/BlackConeGlassWalls3.gltf?v=1654271441980'
-    })
-         .aAssetItem({
-      id: 'GlassWall4',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/BlackConeGlassWalls4.gltf?v=1654271441837'
-    })
-         .aAssetItem({
-      id: 'GlassWall5',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/BlackConeGlassWalls5.gltf?v=1654271441586'
-    })
-         .aAssetItem({
-      id: 'GlassWall6',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/BlackConeGlassWalls6.gltf?v=1654271441555'
-    })
+             
          .aAssetItem({
     id: 'Navigmesh',
       src: '/scene/spiralNavMesh.gltf'
     }) 
-         .aAssetItem({
-    id: 'NavigmeshSphere',
-      src: '/scene/spiralSphereNavMesh.gltf'
-    })    
-         
-         .aAssetItem({
-      id: 'X-program',
-      src: 'https://cdn.glitch.global/88fe0f09-858a-449e-936d-4ec30e06f635/X-prog.gltf?v=1650704573096'
-    })
-         .aAssetItem({
-      id: 'SphereGlassOuter',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/SpiralSphereGlassOuter.gltf?v=1652983030072',
-    
-    })
-  .aAssetItem({
-      id: 'GlassOuter',
-      src: 'https://cdn.glitch.global/07dee895-de9d-44b6-8eb8-3985d37cefa6/GlassOuter.gltf?v=1652719035424'
-    }))
+         )
   
   scene(oom
     
     .aEntity({ id: 'Navigationmesh', navMesh: true, visible: false,  gltfModel: '#Navigmesh', position: '0 0 0', scale: '2 2 2'})
         // .aEntity({ id: 'NavigationmeshSphere', navMesh: true, visible: false,  gltfModel: '#NavigmeshSphere', position: '0 0 0', scale: '2 2 2'})
-    .aEntity({ gltfModel: '#HallDeconstruct', position: '0 0 0', scale: '2 2 2', enmap:'' })
+    .aEntity({ gltfModel: '#BlackBox', position: '0 0 0', scale: '2 2 2', enmap:'' })
     // .aEntity({ gltfModel: '#HallDeconstruct2floor', position: '0 0 0', scale: '2 2 2', enmap:'' })
   .aEntity({ gltfModel: '#Glass', position: '0 0 0', scale: '2 2 2', enmapinner:''})
   // .aEntity({ gltfModel: '#GlassOuter', position: '0 0 0', scale: '2 2 2', enmap:''})
