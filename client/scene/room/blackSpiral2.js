12214a5ab3046e01014aa74de7296ef449880507
Glitch (slender-groovy-aquarius) <none>
2023-01-27 11:36:29 +0000

    🚉🚈 Checkpoint ./client/scene/aframe-avatars.js:36613497/8 ./.glitch-assets:36613497/380 ./client/scene/room/blackSpiral2.js:36613497/206

--- client/scene/room/blackSpiral2.js
+++ client/scene/room/blackSpiral2.js
@@ -21,7 +21,7 @@ export function markup(
     })
          .aAssetItem({
       id: 'Numbers',
-      src: 'https://cdn.glitch.global/acdfc730-297b-4c52-b068-50f3904fbb54/GalleryNumbersBake.glb?v=1674816837099'
+      src: 'https://cdn.glitch.global/acdfc730-297b-4c52-b068-50f3904fbb54/GalleryNumbersBakeHalf.glb?v=1674819359398'
     })
     //      .aAssetItem({
     //   id: 'OuterGlassWindow',
