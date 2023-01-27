98039a3b9c156e95b34e884b6765d7c7ba01ccf9
Glitch (slender-groovy-aquarius) <none>
2023-01-27 11:00:30 +0000

    🌲🛰 Checkpoint ./.glitch-assets:36613497/368 ./client/scene/room/blackSpiral2.js:36613497/320

--- client/scene/room/blackSpiral2.js
+++ client/scene/room/blackSpiral2.js
@@ -19,10 +19,10 @@ export function markup(
       id: 'Glass',
       src: 'https://cdn.glitch.global/acdfc730-297b-4c52-b068-50f3904fbb54/Circle%20GalleryGlass.glb?v=1674765556346'
     })
-    //      .aAssetItem({
-    //   id: 'InnerGlassWindow',
-    //   src: 'https://cdn.glitch.global/acdfc730-297b-4c52-b068-50f3904fbb54/Circle%20GalleryGlass.glb?v=1674762146261'
-    // })
+         .aAssetItem({
+      id: 'Numbers',
+      src: 'https://cdn.glitch.global/acdfc730-297b-4c52-b068-50f3904fbb54/GalleryNumbersBake.glb?v=1674816837099'
+    })
     //      .aAssetItem({
     //   id: 'OuterGlassWindow',
     //   src: 'https://cdn.glitch.global/b1780a83-15c5-4154-bd07-deed2726dd55/OuterGlassWindow.glb?v=1674565674007'
@@ -47,13 +47,13 @@ export function markup(
   
   scene(oom
     
-    .aEntity({ id: 'Navigationmesh', navMesh: true, visible: false,  gltfModel: '#Navigmesh', position: '0 0 0', scale: '1 1 1'})
+    .aEntity({ id: 'Navigationmesh', navMesh: true, visible: false,  gltfModel: '#Navigmesh', position: '0 0 0', scale: '2 2 2'})
         // .aEntity({ id: 'NavigationmeshSphere', navMesh: true, visible: false,  gltfModel: '#NavigmeshSphere', position: '0 0 0', scale: '2 2 2'})
-    .aEntity( { gltfModel: '#BlackBox', position: '0 0 0', scale: '1 1 1'})
+    .aEntity( { gltfModel: '#BlackBox', position: '0 0 0', scale: '2 2 2'})
         
-        .aEntity({ gltfModel: '#Glass', position: '0 0 0', scale: '1 1 1', enmap:''})
+        .aEntity({ gltfModel: '#Glass', position: '0 0 0', scale: '2 2 2', enmap:''})
         
-        // .aEntity({ gltfModel: '#InnerGlassWindow', position: '0 0 0', scale: '0.5 0.5 0.5', enmap:''})
+        .aEntity({ gltfModel: '#Numbers', position: '0 0 0', scale: '2 2 2', enmap:''})
         // .aEntity({ gltfModel: '#OuterGlassWindow', position: '0 0 0', scale: '0.5 0.5 0.5', enmapinner:''})
         
         // .aEntity({ id: 'HRes', gltfModel: '#HiResImage', rotation: '0 90 0', position: '9.4 3.35 -19.53', scale: '4 4 4'})
